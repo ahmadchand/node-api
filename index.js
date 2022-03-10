07b7ff5a0f12d83da7eb7deda4a3654912e2a627
Muhammad Ahmad Chand <emahmadchand@gmail.com>
2022-03-10 15:57:00 +0500

    mongoose - modling relationship

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
+const movies = require("./routes/movies");
 const customers = require("./routes/customers");
+const rentals = require("./routes/rentals");
 const express = require("express");
 const app = express();
 
@@ -11,7 +15,9 @@ mongoose
 
 app.use(express.json());
 app.use("/api/genres", genres);
+app.use("/api/movies", movies);
 app.use("/api/customers", customers);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
